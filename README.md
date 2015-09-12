c167184c496a707ab4f201a7c6aa4867a608409d
Jack Obeney <jackobeney@googlemail.com>
2015-09-12 20:52:58 +0100

    updated readme and fixed problem with logo

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-### website
+## Essex Elite
+
+#### To Do
+* Field validation for contact forms
+* Timetable page
+* What we offer page
--- client/routes.js
+++ client/routes.js
@@ -2,6 +2,10 @@ Router.route('/', function () {
   this.render('about');
 });
 
+Router.route('/coaching', function () {
+  this.render('what-we-offer');
+});
+
 Router.route('/timetable', function () {
   this.render('timetable');
 });
